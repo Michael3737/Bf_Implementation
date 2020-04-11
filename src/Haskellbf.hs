1b27c44fae0baf4310b6d732e953123dbfa71197
Michael3737 <michaelharrison6@virginmedia.com>
2020-04-11 19:50:42 +0100

    First implementation, run function works within console with the help of GHCI. No IO currently

---
+++ src/Haskellbf.hs
@@ -0,0 +1,56 @@
+-- Lets give this a try
+import Data.Char
+
+blankTape :: [Int]
+blankTape = take 1024 [0,0..]
+
+run :: String -> String
+run xs = snd (evaluate (filter isInstruction xs) (blankTape,""))
+
+isInstruction :: Char -> Bool
+isInstruction c
+  | c == '>' || c == '<' || c == '+' || c == '-' || c == '.' || c == '[' || c == ']'= True
+  | otherwise = False
+
+evaluate :: String -> ([Int],String) -> ([Int],String)
+evaluate _ ([],r)                    = error"no tape assigned"
+evaluate [] (xs,r)                   = (xs,r)
+evaluate (ch:str) (xs,r) | ch == '>' = evaluate str ((moveRight xs),r)
+                         | ch == '<' = evaluate str ((moveLeft xs),r)
+                         | ch == '+' = evaluate str ((increment xs),r)
+                         | ch == '-' = evaluate str ((decrement xs),r)
+                         | ch == '.' = evaluate str (xs,update xs r)
+                         | ch == ',' = evaluate str (xs,r) -- implement me
+                         | ch == '[' && head xs > 0 = evaluate (ch:str) (evaluate (loopContents str) (xs,r))
+                         | ch == '[' = evaluate (beyondLoop str) (xs,r)
+                         | ch == ']' = evaluate str (xs,r)
+                         | otherwise = error "not a valid instruction"
+
+moveRight :: [Int] -> [Int]
+moveRight xs = tail xs ++ [head xs]
+
+moveLeft :: [Int] -> [Int]
+moveLeft xs = last xs : init xs
+
+increment :: [Int] -> [Int]
+increment (x:xs) | x+1 <= 128 = x+1 : xs
+                 | otherwise = 0 : xs
+
+decrement :: [Int] -> [Int]
+decrement (x:xs) | x-1 >= 0 = x-1 : xs
+                 | otherwise = 128 : xs
+
+update :: [Int] -> String -> String
+update xs r = r ++ [(chr (head xs))]
+
+beyondLoop :: String -> String
+beyondLoop [] = []
+beyondLoop (ch:str) | ch == '[' = beyondLoop (beyondLoop str)
+                    | ch == ']' = str
+                    | otherwise = beyondLoop str
+
+loopContents :: String -> String
+loopContents [] = []
+loopContents (ch:str) | ch == ']' = []
+                      | ch == '[' = "[" ++ loopContents str ++ "]" ++ loopContents (beyondLoop str)
+                      | otherwise = [ch] ++ loopContents str
