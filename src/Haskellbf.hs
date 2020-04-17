79d370ec9dea3f78bdbb2e7b620a90d1878838dd
Michael3737 <michaelharrison6@virginmedia.com>
2020-04-17 12:45:25 +0100

    First IO implementation. Tests pass but . or , operators within loops cannnot be evaluated correctly

--- src/Haskellbf.hs
+++ src/Haskellbf.hs
@@ -1,32 +1,60 @@
 import Data.Char
 
+main :: IO()
+main = do
+  putStrLn "Please enter your bf code"
+  instructions <- getLine
+  let pureInstructions = filter isInstruction instructions
+      memory = tapeNeeded (length instructions)
+
+  executionLoop (pureInstructions,memory)
+
+executionLoop :: (String,[Int]) -> IO()
+executionLoop (str,xs) = do
+  let output = evaluate (str,xs)
+
+  if fst output == "" || fst output == "," then
+    return ()
+    else if (head (fst output)) == '.' then do
+      putChar (chr . head . snd $ output)
+      executionLoop (tail . fst $ output, snd output)
+    else if (head . fst $ output) == ',' then do
+      putStrLn "Please enter an integer number to insert into the current cell"
+      input <- numPrompt
+      executionLoop (tail . fst $ output,(input : (tail . snd $ output)))
+    else do
+      putStrLn "ahhh"
+      return ()
+
+numPrompt :: Read a => IO a
+numPrompt = do
+  line <- getLine
+  return (read line)
+
 blankTape :: [Int]
 blankTape = [0,0..]
-
-run :: String -> String
-run xs = snd (evaluate (filter isInstruction xs) (tapeNeeded (length xs),"")) -- returning the second of the tuple, to be solved with IO
 
 isInstruction :: Char -> Bool
 isInstruction c
   | c == '>' || c == '<' || c == '+' || c == '-' || 
-    c == '.' || c == '[' || c == ']' || c == ','= True -- no implementation for , yet
+    c == '.' || c == '[' || c == ']' || c == ','= True
   | otherwise = False
 
 tapeNeeded :: Int -> [Int]
 tapeNeeded x = take x blankTape
 
-evaluate :: String -> ([Int],String) -> ([Int],String)
-evaluate _ ([],r)                    = error"no tape assigned"
-evaluate [] (xs,r)                   = (xs,r)
-evaluate (ch:str) (xs,r) | ch == '>' = evaluate str ((moveRight xs),r)
-                         | ch == '<' = evaluate str ((moveLeft xs),r)
-                         | ch == '+' = evaluate str ((increment xs),r)
-                         | ch == '-' = evaluate str ((decrement xs),r)
-                         | ch == '.' = evaluate str (xs,update xs r)
-                         | ch == ',' = evaluate str (xs,r) -- implement me
-                         | ch == '[' && head xs > 0 = evaluate (ch:str) (evaluate (loopContents str) (xs,r))
-                         | ch == '[' = evaluate (beyondLoop str) (xs,r)
-                         | ch == ']' = evaluate str (xs,r)
+evaluate :: (String,[Int]) -> (String,[Int])
+evaluate (_,[])                    = error"no tape assigned"
+evaluate ([],xs)                   = ([],xs)
+evaluate ((ch:str),(xs)) | ch == '>' = evaluate (str,(moveRight xs))
+                         | ch == '<' = evaluate (str,(moveLeft xs))
+                         | ch == '+' = evaluate (str,(increment xs))
+                         | ch == '-' = evaluate (str,(decrement xs))
+                         | ch == '.' = (ch:str,xs)
+                         | ch == ',' = (ch:str,xs)
+                         | ch == '[' && head xs > 0 = evaluate ((ch:str),snd (evaluate ((loopContents str),xs)))
+                         | ch == '[' = evaluate (beyondLoop str,xs)
+                         | ch == ']' = evaluate (str,xs)
                          | otherwise = error "not a valid instruction"
 
 moveRight :: [Int] -> [Int]
@@ -42,9 +70,6 @@ increment (x:xs) | x+1 <= 128 = x+1 : xs
 decrement :: [Int] -> [Int]
 decrement (x:xs) | x-1 >= 0 = x-1 : xs
                  | otherwise = 128 : xs
-
-update :: [Int] -> String -> String
-update xs r = r ++ [(chr (head xs))]
 
 beyondLoop :: String -> String
 beyondLoop [] = []
